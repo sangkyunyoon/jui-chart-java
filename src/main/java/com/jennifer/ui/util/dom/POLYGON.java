f18c425b3d1d5ff59880ec15f24d93fccceb89f5
jinho park <cyberuls@gmail.com>
2014-10-28 10:46:42 +0900

    add svg element

---
+++ src/main/java/com/jennifer/ui/util/dom/POLYGON.java
@@ -0,0 +1,31 @@
+package com.jennifer.ui.util.dom;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Polygon extends Transform {
+
+    private JSONArray orders = new JSONArray();
+
+    public Polygon(JSONObject o) {
+        super("polygon", o);
+    }
+
+    public Polygon() {
+        super("polygon");
+    }
+
+    public Polygon point(double x, double y) {
+        orders.put(x + "," + y);
+
+        return this;
+    }
+
+    @Override
+    public String render() {
+
+        put("points", join(orders, "-"));
+
+        return super.render();
+    }
+}
---
+++ src/main/java/com/jennifer/ui/util/dom/SVG.java
@@ -0,0 +1,31 @@
+package com.jennifer.ui.util.dom;
+
+import com.jennifer.ui.util.DomUtil;
+import org.json.JSONObject;
+
+public class Svg extends DomUtil{
+
+    public Svg(JSONObject attr) {
+        super("svg", attr);
+    }
+
+    public Svg() {
+        super("svg");
+    }
+
+    @Override
+    public void init() {
+        put("xmlns", "http://www.w3.org/2000/svg");
+    }
+
+    public String toXml() {
+        // xml 리턴
+        String str = "<?xml version='1.1' encoding='utf-8' ?>\r\n<!DOCTYPE svg>\r\n";
+        return str + toString();
+    }
+
+    public void save(String path) {
+        // 파일 저장
+    }
+
+}
